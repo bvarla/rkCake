a432b6baa2e45d6597c7c9ecb95d3e30e56c1966
BharatRaj <bharat.varla1@gmail.com>
2017-12-10 23:29:26 -0500

    added ignore file

--- assests/scripts/App.js
+++ assests/scripts/App.js
@@ -1,7 +1,12 @@
+import $ from 'jquery'
 import MobileMenu from './modules/MobileMenu';
 import FlavourSelection from './modules/flavourSelection';
 import Testimonials from './modules/testimonials';
+import RevealOnScroll from './modules/RevealOnScroll';
 
 var mobileMenu = new MobileMenu();
 var flavourSelection = new FlavourSelection();
 var testimonials = new Testimonials();
+var revealOnScrollTesti = new RevealOnScroll($(".mySlides-Testi"),"85%");
+var revealOnScrollContact = new RevealOnScroll($(".mySlides-Testi"),"85%");
+
